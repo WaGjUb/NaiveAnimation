92089e2e09a9fc3a5da25ad9f6abd839cb9c7ff6
rickftime <rickftime@gmail.com>
2016-12-03 15:31:11 -0200

    Adicionado suporte a obj

--- globais.py
+++ globais.py
@@ -6,9 +6,9 @@ yrot = 0            # y rotacao
 
 #Indicam a tranlacao do mundo 
 #(ilusao de mover a camera)
-xtrans =0
-ytrans = 0
-ztrans = 0
+xtrans = -1
+ytrans = -1
+ztrans = -5
 
 #tamanho do passo dado
 passo = 0.01
@@ -16,3 +16,8 @@ passo_rotacao = 0.3 #graus
 
 janela = None
 rtri = 0
+
+# referencias dos objs
+# devem ser criados quando tivermos o contexto do opengl
+chaleira = None
+planta = None
--- main.py
+++ main.py
@@ -5,28 +5,33 @@ import time             # Module for sleeping.
 from math import *            # Module for trigonometric functions.
 from gl_functions import *
 from globais import *
+from objloader import *
 import sys            
 
 
 def main():
-    global janela
-    #Inicia o OpenGl
-    glutInit(sys.argv)
-    glutInitDisplayMode(GLUT_RGBA|GLUT_DOUBLE|GLUT_DEPTH|GLUT_ALPHA)
-    glutInitWindowSize(640, 480)
-    janela = glutCreateWindow("Projeto de CG")
+	global janela
 
-    #Registra os callbacks
-    glutDisplayFunc(desenharCena)   #funcao de desenho
-    glutIdleFunc(desenharCena)      #funcao quando em espera    
-    glutReshapeFunc(redimensionar)  #funcao de redimensionamento
-    glutKeyboardFunc(teclaApertada) #funcao de quando uma tecla e apertada
-    glutKeyboardUpFunc(teclaSolta)  #funcao de quando uma tecla e solta
-    glutIdleFunc(desenharCena)
-    #Inicializa a Janela
-    InitGL(640, 480)
-    #Inicia a execucao
-    glutMainLoop()
+	#Inicia o OpenGl
+	glutInit(sys.argv)
+	glutInitDisplayMode(GLUT_RGBA|GLUT_DOUBLE|GLUT_DEPTH|GLUT_ALPHA)
+	glutInitWindowSize(640, 480)
+	janela = glutCreateWindow("Projeto de CG")
+
+	globais.chaleira = ObjLoader("res/obj/teapot.obj")
+	globais.planta = ObjLoader("res/obj/planta.obj")
+
+	#Inicializa a Janela
+	InitGL(640, 480)
+
+	#Registra os callbacks
+	glutDisplayFunc(desenharCena)   #funcao de desenho
+	glutReshapeFunc(redimensionar)  #funcao de redimensionamento
+	glutIdleFunc(desenharCena)      #funcao quando em espera    
+	glutKeyboardFunc(teclaApertada) #funcao de quando uma tecla e apertada
+	glutKeyboardUpFunc(teclaSolta)  #funcao de quando uma tecla e solta
+	#Inicia a execucao
+	glutMainLoop()
 
 if __name__ == "__main__":
-    main()
+	main()
